3b9d3b6cc8e1330703ca992bdd61113cd2f92bd3
Minoru Osuka <minoru.osuka@gmail.com>
2022-08-27 23:24:40 +0900

    Fix tests

--- pyexample/__init__.py
+++ pyexample/__init__.py
@@ -4,6 +4,6 @@ __dist_name__ = __name__
 """str: __dist_name__ is the name of this distribution.
 """
 
-__version__ = importlib.metadata.version(__dist_name__)
+__version__ = importlib.metadata.version(__package__ or __name__)
 """str: __version__ is the version of this distribution.
 """
