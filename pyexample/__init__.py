35118e7c591e9a30adc061ae4ead0f9cb7cbfb02
Minoru Osuka <minoru.osuka@gmail.com>
2022-08-27 23:35:16 +0900

    Fix tests

--- pyexample/__init__.py
+++ pyexample/__init__.py
@@ -1,12 +1,9 @@
-import importlib.metadata
-import sys
-
-sys.path.append("../")
+import pkg_resources
 
 __dist_name__ = __name__
 """str: __dist_name__ is the name of this distribution.
 """
 
-__version__ = importlib.metadata.version(__package__ or __name__)
+__version__ = pkg_resources.get_distribution(__dist_name__).version
 """str: __version__ is the version of this distribution.
 """
